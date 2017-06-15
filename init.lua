aa5bd8a06135df05309f04b8c160248febea7379
Isidor Zeuner <minetest@quidecco.de>
2017-06-15 19:55:47 +0200

    page stub

--- init.lua
+++ init.lua
@@ -1,3 +1,17 @@
+unified_inventory.register_page(
+    "edutest",
+    {
+        get_formspec = function(
+            player
+        )
+            local formspec = "label[0,0;EDUtest]"
+            return {
+                formspec = formspec,
+            }
+        end,
+    }
+)
+
 unified_inventory.register_button(
     "edutest",
     {
