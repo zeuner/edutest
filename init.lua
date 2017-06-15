16c28a9c71542d1f00282bd4091aaa159544568d
Isidor Zeuner <minetest@quidecco.de>
2017-06-15 21:11:54 +0200

    player dropdown

--- init.lua
+++ init.lua
@@ -5,40 +5,51 @@ minetest.register_on_player_receive_fields(
         fields
     )
         print(
-	    "EDUtest received button press"
-	)
-	print(
-	    "EDUtest player: " .. player:get_player_name(
-	    )
-	)
-	print(
-	    "EDUtest form: " .. formname
-	)
-	for k,v in pairs(
+            "EDUtest received button press"
+        )
+        print(
+            "EDUtest player: " .. player:get_player_name(
+            )
+        )
+        print(
+            "EDUtest form: " .. formname
+        )
+        for k,v in pairs(
             fields
-	) do
-	    print(
-	        "EDUtest field: " .. k .. " | " .. v
-	    )
+        ) do
+            print(
+                "EDUtest field: " .. k .. " | " .. v
+            )
         end
-	if nil ~= fields[
-	    "edutest_back"
+        if nil ~= fields[
+            "edutest_back"
         ] then
-	    unified_inventory.set_inventory_formspec(
-	        player,
-		"edutest"
-	    )
-	    return true
-	elseif nil ~= fields[
-	    "edutest_freeze"
+            unified_inventory.set_inventory_formspec(
+                player,
+                "edutest"
+            )
+            return true
+        elseif nil ~= fields[
+            "edutest_freeze"
         ] then
-	    local formspec = "size[7,7]"
-	    formspec = formspec .. "label[0,0;EDUtest > Freeze]"
-	    formspec = formspec .. "button[0,2;2,2.5;edutest_back;Back]"
-	    player:set_inventory_formspec(
-	        formspec
+            local formspec = "size[7,7]"
+            formspec = formspec .. "label[0,0;EDUtest > Freeze]"
+            formspec = formspec .. "button[0,1;2,1.5;edutest_back;Back]"
+	    local dropdown = "dropdown[0,2;2;frozen;All students"
+	    edutest.for_all_students(
+	        function(
+		    player,
+		    name
+		)
+		    dropdown = dropdown .. "," .. name
+	        end
 	    )
-	    return true
+	    dropdown = dropdown .. ";1]"
+	    formspec = formspec .. dropdown
+            player:set_inventory_formspec(
+                formspec
+            )
+            return true
         end
         return false
     end
@@ -51,7 +62,7 @@ unified_inventory.register_page(
             player
         )
             local formspec = "label[0,0;EDUtest]"
-	    formspec = formspec .. "button[0,2;2,2.5;edutest_freeze;Freeze]"
+            formspec = formspec .. "button[0,1;2,1.5;edutest_freeze;Freeze]"
             return {
                 formspec = formspec,
             }
