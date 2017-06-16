97b87a136d0d46b9c0380110ea5e3d5c78af09d8
Isidor Zeuner <minetest@quidecco.de>
2017-06-16 15:58:19 +0200

    button handler registry

--- init.lua
+++ init.lua
@@ -21,131 +21,160 @@ local field_debug_dump = function(
     end
 end
 
-minetest.register_on_player_receive_fields(
-    function(
+local button_handlers = {
+    edutest_back = function(
+        player,
+        formname,
+        fields
+    )
+        unified_inventory.set_inventory_formspec(
+            player,
+            "edutest"
+        )
+        return true
+    end,
+    edutest_freeze = function(
+        player,
+        formname,
+        fields
+    )
+        local formspec = "size[7,7]"
+        formspec = formspec .. "label[0,0;EDUtest > Freeze]"
+        formspec = formspec .. "button[0,0.5;2,1.5;edutest_back;Back]"
+        local dropdown = "dropdown[0,2;2;frozen;All students"
+        edutest.for_all_students(
+            function(
+                player,
+                name
+            )
+                dropdown = dropdown .. "," .. name
+            end
+        )
+        dropdown = dropdown .. ";1]"
+        formspec = formspec .. dropdown
+        formspec = formspec .. "button[0,3;2,1.5;edutest_do_freeze;Freeze]"
+        formspec = formspec .. "button[3,3;2,1.5;edutest_do_unfreeze;Unfreeze]"
+        player:set_inventory_formspec(
+            formspec
+        )
+        return true
+    end,
+    edutest_do_freeze = function(
         player,
         formname,
         fields
     )
         local name = player:get_player_name(
         )
-        if nil ~= fields[
-            "edutest_back"
+        if nil == fields[
+            "frozen"
         ] then
-            unified_inventory.set_inventory_formspec(
-                player,
-                "edutest"
+            print(
+                "EDUtest unexpected condition: frozen field empty"
+            )
+            field_debug_dump(
+                name,
+                formname,
+                fields
+            )
+            return false
+        end
+        if "All students" == fields[
+            "frozen"
+        ] then
+            minetest.chatcommands[
+                "every_student"
+            ].func(
+                name,
+                "freeze subject"
             )
             return true
-        elseif nil ~= fields[
-            "edutest_freeze"
+        end
+        minetest.chatcommands[
+            "freeze"
+        ].func(
+            name,
+            fields[
+                "frozen"
+            ]
+        )
+        return true
+    end,
+    edutest_do_unfreeze = function(
+        player,
+        formname,
+        fields
+    )
+        local name = player:get_player_name(
+        )
+        if nil == fields[
+            "frozen"
         ] then
-            local formspec = "size[7,7]"
-            formspec = formspec .. "label[0,0;EDUtest > Freeze]"
-            formspec = formspec .. "button[0,0.5;2,1.5;edutest_back;Back]"
-            local dropdown = "dropdown[0,2;2;frozen;All students"
-            edutest.for_all_students(
-                function(
+            print(
+                "EDUtest unexpected condition: frozen field empty"
+            )
+            field_debug_dump(
+                name,
+                formname,
+                fields
+            )
+            return false
+        end
+        if "All students" == fields[
+            "frozen"
+        ] then
+            minetest.chatcommands[
+                "every_student"
+            ].func(
+                name,
+                "unfreeze subject"
+            )
+            minetest.chatcommands[
+                "every_student"
+            ].func(
+                name,
+                "grant subject interact"
+            )
+            return true
+        end
+        minetest.chatcommands[
+            "unfreeze"
+        ].func(
+            name,
+            fields[
+                "frozen"
+            ]
+        )
+        minetest.chatcommands[
+            "grant"
+        ].func(
+            name,
+            fields[
+                "frozen"
+            ] .. " interact"
+        )
+        return true
+    end,
+}
+
+minetest.register_on_player_receive_fields(
+    function(
+        player,
+        formname,
+        fields
+    )
+        for k, v in pairs(
+            button_handlers
+        ) do
+            if nil ~= fields[
+                k
+            ] then
+                return v(
                     player,
-                    name
-                )
-                    dropdown = dropdown .. "," .. name
-                end
-            )
-            dropdown = dropdown .. ";1]"
-            formspec = formspec .. dropdown
-            formspec = formspec .. "button[0,3;2,1.5;edutest_do_freeze;Freeze]"
-            formspec = formspec .. "button[3,3;2,1.5;edutest_do_unfreeze;Unfreeze]"
-            player:set_inventory_formspec(
-                formspec
-            )
-            return true
-        elseif nil ~= fields[
-            "edutest_do_freeze"
-        ] then
-            if nil == fields[
-                "frozen"
-            ] then
-                print(
-                    "EDUtest unexpected condition: frozen field empty"
-                )
-                field_debug_dump(
-                    name,
                     formname,
                     fields
                 )
-                return false
             end
-            if "All students" == fields[
-                "frozen"
-            ] then
-                minetest.chatcommands[
-                    "every_student"
-                ].func(
-                    name,
-                    "freeze subject"
-                )
-                return true
-            end
-            minetest.chatcommands[
-                "freeze"
-            ].func(
-                name,
-                fields[
-                    "frozen"
-                ]
-            )
-            return true
-        elseif nil ~= fields[
-            "edutest_do_unfreeze"
-        ] then
-            if nil == fields[
-                "frozen"
-            ] then
-                print(
-                    "EDUtest unexpected condition: frozen field empty"
-                )
-                field_debug_dump(
-                    name,
-                    formname,
-                    fields
-                )
-                return false
-            end
-            if "All students" == fields[
-                "frozen"
-            ] then
-                minetest.chatcommands[
-                    "every_student"
-                ].func(
-                    name,
-                    "unfreeze subject"
-                )
-                minetest.chatcommands[
-                    "every_student"
-                ].func(
-                    name,
-                    "grant subject interact"
-                )
-                return true
-            end
-            minetest.chatcommands[
-                "unfreeze"
-            ].func(
-                name,
-                fields[
-                    "frozen"
-                ]
-            )
-            minetest.chatcommands[
-                "grant"
-            ].func(
-                name,
-                fields[
-                    "frozen"
-                ] .. " interact"
-            )
-            return true
         end
         return false
     end
