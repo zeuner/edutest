549277d52a4229260237a87d8c3f9d2cd0815579
Isidor Zeuner <minetest@quidecco.de>
2018-07-06 21:51:31 +0200

    distinguish different UI handler contexts

--- init.lua
+++ init.lua
@@ -328,6 +328,8 @@ local new_main_form = function(
             )
             button_handlers[
                 name
+            ][
+                "inventory"
             ] = nil
             local old_page = player_previous_inventory_page[
                 name
@@ -349,22 +351,39 @@ end
 
 local set_current_form_handlers = function(
     player,
-    form
+    form,
+    context
 )
+    local installed_context = context
+    if not installed_context then
+        installed_context = "inventory"
+    end
     local name = player:get_player_name(
     )
+    if not button_handlers[
+        name
+    ] then
+        button_handlers[
+            name
+        ] = {
+        }
+    end
     button_handlers[
         name
+    ][
+        installed_context
     ] = {
     }
-    for k, v in pairs(
+    for field, action in pairs(
         form.handlers
     ) do
         button_handlers[
             name
         ][
-            k
-        ] = v
+            installed_context
+        ][
+            field
+        ] = action
     end
 end
 
@@ -1553,23 +1572,27 @@ local on_player_receive_fields = function(
 )
     local name = player:get_player_name(
     )
-    local handlers = button_handlers[
+    local contexts = button_handlers[
         name
     ]
-    if not handlers then
+    if not contexts then
         return false
     end
-    for k, v in pairs(
-        handlers
+    for context, handlers in pairs(
+        contexts
     ) do
-        if nil ~= fields[
-            k
-        ] then
-            return v(
-                player,
-                formname,
-                fields
-            )
+        for field, action in pairs(
+            handlers
+        ) do
+            if nil ~= fields[
+                field
+            ] then
+                return action(
+                    player,
+                    formname,
+                    fields
+                )
+            end
         end
     end
     return false
