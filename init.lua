fd137634b42bc2fe9fd260ee1b8a3adc3de7677e
Isidor Zeuner <minetest@quidecco.de>
2021-03-30 15:55:56 +0200

    add/remove teachers

--- init.lua
+++ init.lua
@@ -480,6 +480,10 @@ local choose_student_entry = S(
     "Choose student"
 )
 
+local choose_teacher_entry = S(
+    "Choose teacher"
+)
+
 local choose_group_entry = S(
     "Choose group"
 )
@@ -536,6 +540,59 @@ local basic_student_dropdown = function(
             entries
         )
         edutest.for_all_students(
+            function(
+                player,
+                name
+            )
+                local width = string_width(
+                    name
+                )
+                if max_width < width then
+                    max_width = width
+                end
+                current_index = current_index + 1
+                if selected_value == name then
+                    selected_index = current_index
+                end
+                entries = entries .. "," .. name
+            end
+        )
+        local height = 1.5
+        return "dropdown[" .. layout:region_position(
+            max_width,
+            height
+        ) .. ";" .. max_width .. ";" .. field .. ";" .. entries .. ";" .. selected_index .. "]"
+    end
+end
+
+local basic_teacher_dropdown = function(
+    field
+)
+    return function(
+        layout,
+        data
+    )
+        local selected_value = ""
+        local selected_index = 1
+        local current_index = 1
+        if data then
+            if data[
+                field
+            ] then
+                selected_value = data[
+                    field
+                ]
+            end
+        end
+        local entry = choose_teacher_entry
+        if selected_value == entry then
+            selected_index = current_index
+        end
+        local entries = entry
+        local max_width = string_width(
+            entries
+        )
+        edutest.for_all_teachers(
             function(
                 player,
                 name
@@ -1698,6 +1755,200 @@ else
         )
     )
 end
+
+main_menu_form:add_button(
+    main_layout,
+    main_menu_form:new_field(
+    ),
+    S(
+        "Add teacher"
+    ),
+    function(
+        player,
+        formname,
+        fields,
+        form,
+        field
+    )
+        local subform = form.resources[
+            field
+        ].form
+        set_current_inventory_form(
+            player,
+            subform
+        )
+        return true
+    end,
+    function(
+    )
+        local form = new_sub_form(
+            "EDUtest > " .. S(
+                "Add teacher"
+            )
+        )
+        local subject = form:new_field(
+        )
+        form:add_input(
+            static_layout(
+                "0,2"
+            ),
+            basic_student_dropdown(
+                subject
+            ),
+            subject
+        )
+        form:add_button(
+            static_layout(
+                "0,3"
+            ),
+            form:new_field(
+            ),
+            S(
+                "Grant teacher privilege"
+            ),
+            function(
+                player,
+                formname,
+                fields
+            )
+                local name = player:get_player_name(
+                )
+                if false == check_field(
+                    name,
+                    formname,
+                    fields,
+                    subject
+                ) then
+                    return false
+                end
+                if choose_student_entry == fields[
+                    subject
+                ] then
+                    return false
+                end
+                local subject_player = minetest.get_player_by_name(
+                    fields[
+                        subject
+                    ]
+                )
+                if not subject_player then
+                    minetest.log(
+                        "action",
+                        "player " .. fields[
+                            subject
+                        ] .. " disappeared"
+                    )
+                    return false
+                end
+                edutest.student_to_teacher(
+                    player,
+                    subject_player
+                )
+                return true
+            end
+        )
+        return {
+            form = form
+        }
+    end
+)
+
+main_menu_form:add_button(
+    main_layout,
+    main_menu_form:new_field(
+    ),
+    S(
+        "Remove teacher"
+    ),
+    function(
+        player,
+        formname,
+        fields,
+        form,
+        field
+    )
+        local subform = form.resources[
+            field
+        ].form
+        set_current_inventory_form(
+            player,
+            subform
+        )
+        return true
+    end,
+    function(
+    )
+        local form = new_sub_form(
+            "EDUtest > " .. S(
+                "Remove teacher"
+            )
+        )
+        local subject = form:new_field(
+        )
+        form:add_input(
+            static_layout(
+                "0,2"
+            ),
+            basic_teacher_dropdown(
+                subject
+            ),
+            subject
+        )
+        form:add_button(
+            static_layout(
+                "0,3"
+            ),
+            form:new_field(
+            ),
+            S(
+                "Revoke teacher privilege"
+            ),
+            function(
+                player,
+                formname,
+                fields
+            )
+                local name = player:get_player_name(
+                )
+                if false == check_field(
+                    name,
+                    formname,
+                    fields,
+                    subject
+                ) then
+                    return false
+                end
+                if choose_teacher_entry == fields[
+                    subject
+                ] then
+                    return false
+                end
+                local subject_player = minetest.get_player_by_name(
+                    fields[
+                        subject
+                    ]
+                )
+                if not subject_player then
+                    minetest.log(
+                        "action",
+                        "player " .. fields[
+                            subject
+                        ] .. " disappeared"
+                    )
+                    return false
+                end
+                edutest.teacher_to_student(
+                    player,
+                    subject_player
+                )
+                return true
+            end
+        )
+        return {
+            form = form
+        }
+    end
+)
 
 main_menu_form:add_button(
     main_layout,
